829daad3056cc35d19376e88edbcf800b8702a28
svanzee4cb <svanzee@Van-Zees-MacBook-Pro.local>
2015-03-12 10:16:22 -0500

    cleaned up unnecessary lines in attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,4 @@
 # NOTE!!!!  CHECK SECUTIRY SETTINGS FOR THE DB!!!!!!
-
 default['yum']['main']['obsoletes'] = false
 default[:mongodb][:package_version] = '2.4.9-mongodb_1'
 default[:mongodb][:cluster_name] = 'vzdev'
@@ -8,4 +7,3 @@ default['mongodb']['config']['fork'] = 'true'
 default['mongodb']['config']['dbpath'] = '/data/cfgsvr/db'
 default['mongodb']['config']['logpath'] = '/data/cfgsvr/log/config.log'
 default['mongodb']['is_configserver'] = true
-# default['mongodb']['config']['configsvr'] = true
